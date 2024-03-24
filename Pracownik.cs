dcdc5d017c4968b50075fb4a7ff7dc22882beda7
Bartosz Potyrała <bartosz.potyrala@microsoft.wsei.edu.pl>
2024-03-24 12:44:27 +0100

    e

---
+++ Pracownik.cs
@@ -0,0 +1,65 @@
+﻿using ProjektZaj2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjektZaj2
+{
+    public abstract class Pracownik
+    {
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+        public string Stanowisko { get; set; }
+        public decimal Wynagrodzenie { get; set; }
+
+        public abstract void PokazInformacje();
+        public virtual decimal ObliczRoczneWynagrodzenie()
+        {
+            return Wynagrodzenie * 12;
+        } 
+    }
+}
+
+public class PracownikZdalny : Pracownik
+{
+    public int IloscDniZdalnych { get; set; }
+
+    public virtual decimal ObliczRoczneWynagrodzenie()
+    {
+        return Wynagrodzenie * 12;
+    }
+    public override void PokazInformacje()
+    {
+        Console.WriteLine($"Imie: {Imie}, Nazwisko: {Nazwisko}, Stanowisko: {Stanowisko}, Wynagrodzenie: {Wynagrodzenie}, Ilosc dni zdalnych: {IloscDniZdalnych}");
+        Console.WriteLine("\r\n");
+    }
+}
+
+public class PracownikBiurowy : Pracownik
+{
+    public int IloscGodzinPracy { get; set; }
+
+    public virtual decimal ObliczRoczneWynagrodzenie()
+    {
+        return Wynagrodzenie * 12;
+    }
+    public override void PokazInformacje()
+    {
+        Console.WriteLine($"Imie: {Imie}, Nazwisko: {Nazwisko}, Stanowisko: {Stanowisko}, Wynagrodzenie: {Wynagrodzenie}, Ilosc godzin pracy: {IloscGodzinPracy}");
+        Console.WriteLine("\r\n");
+    }
+
+}
+
+public class Menadzer : Pracownik
+{
+    public int BonusRoczny { get; set; }
+
+    public override void PokazInformacje()
+    {
+        Console.WriteLine($"Imie: {Imie}, Nazwisko: {Nazwisko}, Stanowisko: {Stanowisko}, Wynagrodzenie: {Wynagrodzenie}, Bonus roczny: {BonusRoczny}");
+        Console.WriteLine("\r\n");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using ProjektZaj2;
+using System;
+
+//namespace Diagram
+
+    class Program
+{
+    static void Main()
+    {
+        var pracownikZdalny = new PracownikZdalny
+        {
+            Imie = "Tomek",
+            Nazwisko = "Kokoszka",
+            Stanowisko = "Programista",
+            Wynagrodzenie = 12000,
+            IloscDniZdalnych = 20
+        };
+
+        var pracownikBiurowy = new PracownikBiurowy
+        {
+            Imie = "Robert",
+            Nazwisko = "Lewandowski",
+            Stanowisko = "Kopacz",
+            Wynagrodzenie = 500000,
+            IloscGodzinPracy = 10
+        };
+        var menadzer = new Menadzer
+        {
+            Imie = "Maciej",
+            Nazwisko = "Konop",
+            Stanowisko = "Menadzer",
+            Wynagrodzenie = 20000,
+            BonusRoczny = 5000
+        };
+
+        pracownikZdalny.PokazInformacje();
+        pracownikBiurowy.PokazInformacje();
+        menadzer.PokazInformacje();
+    }
+}
